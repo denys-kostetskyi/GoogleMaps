c7041545aa92854a3341631b51dc0df52dcc3418
Denys Kostetskyi <denys.kostetskyi@globallogic.com>
2024-08-22 15:31:57 +0300

    Added binding in MainActivity and maps metadata in AndroidManifest.xml

--- app/src/main/java/com/denyskostetskyi/maps/MainActivity.kt
+++ app/src/main/java/com/denyskostetskyi/maps/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.denyskostetskyi.maps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding ?: throw RuntimeException("ActivityMainBinding is null")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
 }
